025391bb33c68feabaa160be55527b5cbd3857b5
rvian003 <rvian003@fiu.edu>
2015-11-24 09:49:56 -0500

    Playing with tutorial

--- Tutorial/greeter.ts
+++ Tutorial/greeter.ts
@@ -1,7 +1,42 @@
-function greeter (person) {
-	return "Hello, " + person
+//function greeter (person) {
+//        return "Hello, " + person
+//}
+
+//var user  = "Jane User";
+
+//document.body.innerHTML = greeter(user);
+
+
+//Using Type Annotation simple test
+//function greeter (person: string) {
+//	return "Hello, " + person
+//}
+
+//var user  = [1,2,3];
+
+//document.body.innerHTML = greeter(user);
+
+
+interface Person {
+	firstname: string;
+	lastname: string;
 }
 
-var user  = "Jane User";
+function greeter (person: Person)
+{
+ 	return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = {firstname: "Jane", lastname: "User"};
 
 document.body.innerHTML = greeter(user);
+
+
+
+
+
+
+
+
+
+
