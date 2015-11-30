776de6a87c8aa2e635df227d3c0cd61a0db886b2
rvian003 <rvian003@fiu.edu>
2015-11-30 15:35:17 -0500

    TypeScript Classes

--- Tutorial/Classes.js
+++ Tutorial/Classes.js
@@ -8,19 +8,18 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 //Classes are defined using class name, properties, constructor, and methods. 
 //Example
-// class Greeter {
-// 	//Property
-//     greeting: string;
-// 	//Constructor
-//     constructor(message: string) {
-//         this.greeting = message;
-//     }
-// 	//Method
-//     greet() {
-//         return "Hello, " + this.greeting;
-//     }
-// }
-// var greeter = new Greeter("world");
+var Greeter = (function () {
+    //Constructor
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    //Method
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+})();
+var greeter = new Greeter("world");
 //Note: We can use inheritance as well in typescript
 var Animal = (function () {
     function Animal(theName) {
@@ -28,7 +27,7 @@ var Animal = (function () {
     }
     Animal.prototype.move = function (meters) {
         if (meters === void 0) { meters = 0; }
-//        alert(this.name + " moved " + meters + "m.");
+        alert(this.name + " moved " + meters + "m.");
     };
     return Animal;
 })();
@@ -39,9 +38,8 @@ var Snake = (function (_super) {
     }
     Snake.prototype.move = function (meters) {
         if (meters === void 0) { meters = 5; }
-//        alert("Slithering...");
-        console.log("Slithering");
-	_super.prototype.move.call(this, meters);
+        alert("Slithering...");
+        _super.prototype.move.call(this, meters);
     };
     return Snake;
 })(Animal);
@@ -52,7 +50,7 @@ var Horse = (function (_super) {
     }
     Horse.prototype.move = function (meters) {
         if (meters === void 0) { meters = 45; }
-//        alert("Galloping...");
+        alert("Galloping...");
         _super.prototype.move.call(this, meters);
     };
     return Horse;
@@ -61,3 +59,81 @@ var sam = new Snake("Sammy the Python");
 var tom = new Horse("Tommy the Palomino");
 sam.move();
 tom.move(34);
+//When comparing types that have 'private' members, we treat these differently. 
+//For two types to be considered compatible, if one of them has a private member, 
+//then the other must have a private member that originated in the same declaration.
+var Animal = (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    return Animal;
+})();
+var Rhino = (function (_super) {
+    __extends(Rhino, _super);
+    function Rhino() {
+        _super.call(this, "Rhino");
+    }
+    return Rhino;
+})(Animal);
+var Employee = (function () {
+    function Employee(theName) {
+        this.name = theName;
+    }
+    return Employee;
+})();
+var animal = new Animal("Goat");
+var rhino = new Rhino();
+var employee = new Employee("Bob");
+animal = rhino;
+animal = employee; //error: Animal and Employee are not compatible
+//Note that the error is occurring because the private property was not declared on the same declaration meaning there was one declaration for the name in animal
+//and another declaration for the name in employee. This didn't happen to rhino since the only declaration of the private variable is made in the animal class
+//Youy ca also drop the property declaration and only include it in the contructor as parameters. Bellow is an example of said change 
+//Before
+var Animal = (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    Animal.prototype.move = function (meters) {
+        alert(this.name + " moved " + meters + "m.");
+    };
+    return Animal;
+})();
+//After
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (meters) {
+        alert(this.name + " moved " + meters + "m.");
+    };
+    return Animal;
+})();
+//If you want to limit the way a user access a variable you can implement set and get methods to modify or get properties values that are private.
+//Example
+var passcode = "secret passcode";
+var Employee = (function () {
+    function Employee() {
+    }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                alert("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee;
+})();
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
--- Tutorial/Classes.ts
+++ Tutorial/Classes.ts
@@ -4,20 +4,20 @@
 
 //Classes are defined using class name, properties, constructor, and methods. 
 //Example
-// class Greeter {
-// 	//Property
-//     greeting: string;
-// 	//Constructor
-//     constructor(message: string) {
-//         this.greeting = message;
-//     }
-// 	//Method
-//     greet() {
-//         return "Hello, " + this.greeting;
-//     }
-// }
+class Greeter {
+	//Property
+    greeting: string;
+	//Constructor
+    constructor(message: string) {
+        this.greeting = message;
+    }
+	//Method
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
 
-// var greeter = new Greeter("world");
+var greeter = new Greeter("world");
 
 
 //Note: We can use inheritance as well in typescript
@@ -51,3 +51,75 @@ var tom: Animal = new Horse("Tommy the Palomino");
 
 sam.move();
 tom.move(34);
+
+
+//When comparing types that have 'private' members, we treat these differently. 
+//For two types to be considered compatible, if one of them has a private member, 
+//then the other must have a private member that originated in the same declaration.
+
+class Animal {
+    private name:string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+class Rhino extends Animal {
+	constructor() { super("Rhino"); }
+}
+
+class Employee {
+    private name:string;
+    constructor(theName: string) { this.name = theName; }	
+}
+
+var animal = new Animal("Goat");
+var rhino = new Rhino();
+var employee = new Employee("Bob");
+
+animal = rhino;
+animal = employee; //error: Animal and Employee are not compatible
+//Note that the error is occurring because the private property was not declared on the same declaration meaning there was one declaration for the name in animal
+//and another declaration for the name in employee. This didn't happen to rhino since the only declaration of the private variable is made in the animal class
+//Youy ca also drop the property declaration and only include it in the contructor as parameters. Bellow is an example of said change 
+//Before
+class Animal {
+    private name:string;
+    constructor(theName: string) { this.name = theName; }
+	move(meters: number) {
+        alert(this.name + " moved " + meters + "m.");
+    }
+}
+
+//After
+class Animal {
+    constructor(private name: string) { }
+    move(meters: number) {
+        alert(this.name + " moved " + meters + "m.");
+    }
+}
+
+//If you want to limit the way a user access a variable you can implement set and get methods to modify or get properties values that are private.
+//Example
+var passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+	
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            alert("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
